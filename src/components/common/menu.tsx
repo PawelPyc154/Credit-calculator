f1307557f034e08402bd06493fbb0ffe89a0a252
Pawel Pyc <pawel.pyc@techsterowniki.pl>
2025-11-02 17:19:22 +0100

    no message

--- src/components/common/menu.tsx
+++ src/components/common/menu.tsx
@@ -1,19 +1,18 @@
-'use client'
+"use client";
 
-import Tippy, { type TippyProps } from '@aslakson/tippyjs-react'
+import Tippy, { type TippyProps } from "@tippyjs/react";
+import React, { useState } from "react";
 
-import React, { useState } from 'react'
+import "tippy.js/dist/tippy.css";
 
-import 'tippy.js/dist/tippy.css'
-
-import tw from 'tw-tailwind'
+import tw from "tw-tailwind";
 
 export const Menu = ({ content, children, ...props }: TippyProps) => {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-  const show = () => setVisible(true)
+  const show = () => setVisible(true);
 
-  const hide = () => setVisible(false)
+  const hide = () => setVisible(false);
 
   return (
     <Tippy
@@ -27,9 +26,10 @@ export const Menu = ({ content, children, ...props }: TippyProps) => {
       className="rounded-lg! bg-white! p-0! shadow-gray-150 shadow-lg!"
       content={<Content onClick={hide}>{content}</Content>}
     >
-      {children && React.cloneElement(children, { onClick: visible ? hide : show })}
+      {children &&
+        React.cloneElement(children, { onClick: visible ? hide : show })}
     </Tippy>
-  )
-}
+  );
+};
 
-const Content = tw.div`grid gap-2 p-1`
+const Content = tw.div`grid gap-2 p-1`;
